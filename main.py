04f1c3ba442402c8036e5e176f7d9910e3b1dce5
g.luensdorf <g.luensdorf@gmail.com>
2018-01-14 16:09:14 +0100

    added more functions

--- main.py
+++ main.py
@@ -24,5 +24,25 @@ def class __init__(self, g, m, hz, lambda_min, beta_Mb):
     pass
 
 def calc_diffraction_angle(self):
-    self.alpha_m = np.arcsin(self.m * self.)
-    pass
+    # we need to specify 'interval'
+    # we propably need a loop for the interval - need more information on how it will be calculated
+    interval = [self.lambda_min, self.lambda_max]
+    self.alpha_m = np.arcsin((self.m * self.interval / self.g) + np.sin(self.alpha_e))
+    # alpha_m includes diag and vert version of it
+    return alpha_m
+
+def calc_x_0(self):
+    a = 'what am I? how big am I?' # what value does 'a' have? 
+    alpha_m_vert = alpha_m[0]
+    alpha_m_diag = alpha_m[1]
+
+    if alpha_m_vert == self.beta_Mb: 
+        x_0 = [-self.alpha_m_diag, 0, (-self.alpha_m_diag / self.beta_Mb)]
+    else:
+        x_0 = [
+            a * np.arctan(np.sqrt(math.pow(np.tan(self.beta_Mb - alpha_m_vert), 2) + math.pow(np.tan(alpha_m_vert - alpha_m_diag - self.beta_Mb), 2) )),
+            np.arccos((a * np.tan(alpha_m_vert - alpha_m_diag - self.beta_Mb)) / (np.sqrt(math.pow(np.tan(self.beta_Mb - alpa_m_vert)) + 
+                math.pow(np.tan(alpha_m_vert - alpha_m_diag - self.beta_Mb), 2)))),
+            ((alpha_m_vert - alpha_m_diag) / self.beta_Mb) - 1
+        ]
+    return x_0

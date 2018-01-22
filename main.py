calculated values are looking good but the algebraic sign is not correct yet

main.py
@@ -6,6 +6,10 @@ import numpy as np
 
 from wavelength_diffraction_angle import WavelengthDiffractionAngle as wlda
 
+#  Sensorneigung  |   Transformationsparameter  |Wellenl�nge am |Wellenl�nge am
+#   bx   |   by   |  Theta  |    Phi        t   |diagonalen Stab|vertikalen Stab
+# [grad] | [grad] |  [grad] |   [grad]     []   |     [nm]      |    [nm]
+# ------------------------------------------------------------------------------
 # -5.000 | -5.000 |   7.053 |  135.000 | -0.996 |    696.15911  |    696.40479  
 # -2.500 | -2.500 |   3.533 |  135.000 | -0.500 |    630.49157  |    663.66575  
 # g is in micro-meter
@@ -15,8 +19,8 @@ foo = wlda(
     lambda_min=0.4300, beta_Mb=np.radians(5)
     )
 
-foo.calc_diffraction_angle(lambda_diag=0.69640479, lambda_vert=0.69615911) # first sample
-# foo.calc_diffraction_angle(lambda_diag=0.63049157, lambda_vert=0.66366575)
+# foo.calc_diffraction_angle(lambda_vert=0.69640479, lambda_diag=0.69615911) # first sample
+foo.calc_diffraction_angle(lambda_vert=0.66366575, lambda_diag=0.63049157)
 foo.calc_x_0()
 foo.calc_jokabi_matrix()
 foo.determinant_jakobi()
fb10606ce64c9318c120813a4d1d791b7749c17b
g.luensdorf <g.luensdorf@gmail.com>
2018-01-23 21:58:57 +0100

    load file complete, +- error not fixed yet

--- main.py
+++ main.py
@@ -14,22 +14,33 @@ from wavelength_diffraction_angle import WavelengthDiffractionAngle as wlda
 # -2.500 | -2.500 |   3.533 |  135.000 | -0.500 |    630.49157  |    663.66575  
 # g is in micro-meter
 # lambda_min is in nano-meters
-foo = wlda(
-    g=10/13, m=1, h_z=0.769231, 
-    lambda_min=0.4300, beta_Mb=np.radians(5)
-    )
 
-# foo.calc_diffraction_angle(lambda_vert=0.69640479, lambda_diag=0.69615911) # first sample
-foo.calc_diffraction_angle(lambda_vert=0.66366575, lambda_diag=0.63049157)
-foo.calc_x_0()
-foo.calc_jokabi_matrix()
-foo.determinant_jakobi()
-foo.calc_function_equation()
-foo.noname()
+f = open('data/Testwerte 21-12-2017.txt', 'r')
+data = list(f)[13:-1]
+value = []
+for x in data:
+    x.split('|', 6)[-2:]
+    value.append([float(bla) for bla in x.split('|', 6)])
 
-bar = foo.calc_tilting_angle()
-print(bar)
-a = np.degrees(bar[0])
-b = np.degrees(bar[1])
-print(a)
-print(b)
+for pair in value:
+    diag = pair[-2] / 1000
+    vert = pair[-1] / 1000
+    foo = wlda(
+        g=10/13, m=1, h_z=0.769231, 
+        lambda_min=0.4300, beta_Mb=np.radians(5)
+        )
+
+    # foo.calc_diffraction_angle(lambda_vert=0.69640479, lambda_diag=0.69615911) # first sample
+    foo.calc_diffraction_angle(lambda_vert=vert, lambda_diag=diag)
+    foo.calc_x_0()
+    foo.calc_jokabi_matrix()
+    foo.determinant_jakobi()
+    foo.calc_function_equation()
+    foo.noname()
+
+    bar = foo.calc_tilting_angle()
+    print(bar)
+    a = np.degrees(bar[0])
+    b = np.degrees(bar[1])
+    print(a)
+    print(b)

598d12405ee71a684a212326c4d41adb703cfb8b
g.luensdorf <g.luensdorf@gmail.com>
2018-02-01 10:46:52 +0100

    cleaned up main.py, added helper.py

--- main.py
+++ main.py
@@ -3,66 +3,14 @@
 
 import math as math
 import numpy as np
+import helper as hp
 
 from wavelength_diffraction_angle import WavelengthDiffractionAngle as wlda
 
-#  Sensorneigung  |   Transformationsparameter  |Wellenl�nge am |Wellenl�nge am
-#   bx   |   by   |  Theta  |    Phi        t   |diagonalen Stab|vertikalen Stab
-# [grad] | [grad] |  [grad] |   [grad]     []   |     [nm]      |    [nm]
-# ------------------------------------------------------------------------------
-# -5.000 | -5.000 |   7.053 |  135.000 | -0.996 |    696.15911  |    696.40479  
-# -2.500 | -2.500 |   3.533 |  135.000 | -0.500 |    630.49157  |    663.66575  
-# g is in micro-meter
-# lambda_min is in nano-meters
-
-def write_output(_wlda, vert, diag, theta, phi, tau, bx, by, diff=False):
-    if diff:
-        result = f'{abs(bx - np.degrees(_wlda.beta_xy[0])):^+7.3f}|'\
-            f'{abs(by - np.degrees(_wlda.beta_xy[1])):^+8.3f}|'\
-            f'{abs(theta - np.degrees(_wlda.new_theta)):^+9.3f}|'\
-            f'{abs(phi - np.degrees(_wlda.new_phi)):^+10.3f}|'\
-            f'{abs(tau - _wlda.new_tau):^+8.3f}|'\
-            f'{vert*1000:15.5f}|'\
-            f'{diag*1000:15.5f}|'\
-            f'{num_iteration:^10}\n'
-    else:
-        result = f'{np.degrees(_wlda.beta_xy[0]):^+7.3f}|'\
-            f'{np.degrees(_wlda.beta_xy[1]):^+8.3f}|'\
-            f'{np.degrees(_wlda.new_theta):^+9.3f}|'\
-            f'{np.degrees(_wlda.new_phi):^+10.3f}|'\
-            f'{_wlda.new_tau:^+8.3f}|'\
-            f'{vert*1000:15.5f}|'\
-            f'{diag*1000:15.5f}|'\
-            f'{num_iteration:^10}\n'
-    return result
-
-'''
-def write_diff_output(_wlda, vert, diag, theta, phi, tau, bx, by):
-    result = f'{np.degrees(_wlda.beta_xy[0]):^+7.3f}|'\
-        f'{np.degrees(_wlda.beta_xy[1]):^+8.3f}|'\
-        f'{np.degrees(_wlda.new_theta):^+9.3f}|'\
-        f'{np.degrees(_wlda.new_phi):^+10.3f}|'\
-        f'{_wlda.new_tau:^+8.3f}|'\
-        f'{vert*1000:15.5f}|'\
-        f'{diag*1000:15.5f}|'\
-        f'{num_iteration:^10}\n'
-    return result
-'''
-
-f = open('data/Testwerte 21-12-2017.txt', 'r')
-data = list(f)[13:len(list(f))-1]
-f.close()
-value = []
-header = ' Sensorneigung  |   Transformationsparameter  |Wellenl�nge am |Wellenl�nge am |           \n'\
-    '  bx   |   by   |  Theta  |    Phi        t   |diagonalen Stab|vertikalen Stab|Iteration  \n'\
-    '[grad] | [grad] |  [grad] |   [grad]     []   |     [nm]      |    [nm]       |   []      \n'\
-    '------------------------------------------------------------------------------------------\n'
-result = [header]
+data = hp.load_file('data/Testwerte 21-12-2017.txt')
+value = hp.get_values_from_data(data)
+result = [hp.get_header()]
 num_iteration = int()
-
-for x in data:
-    x.split('|', 6)[-2:]
-    value.append([float(bla) for bla in x.split('|', 6)])
 
 for pair in value:
     num_iteration = 0
@@ -79,13 +27,13 @@ for pair in value:
     _wlda.determinant_jakobi()
     _wlda.calc_function_equation()
     _wlda.noname()
-
-    bar = _wlda.calc_tilting_angle()
+    _wlda.calc_tilting_angle()
 
     result.append(
-        write_output(
-            _wlda, vert, diag, pair[2], pair[3], 
-            pair[4], pair[0], pair[1], diff=True
+        hp.format_result(
+            _wlda, vert, diag, 
+            pair[2], pair[3], pair[4], pair[0], pair[1], 
+            num_iteration, diff=True
         )
     )
     """ BEGIN NEXT ITERATION """
@@ -102,23 +50,14 @@ for pair in value:
     _wlda.determinant_jakobi()
     _wlda.calc_function_equation()
     _wlda.noname()
-
-    bar = _wlda.calc_tilting_angle()
+    _wlda.calc_tilting_angle()
 
     result.append(
-        write_output(
-            _wlda, vert, diag, pair[2], pair[3], 
-            pair[4], pair[0], pair[1], diff=True
+        hp.format_result(
+            _wlda, vert, diag, 
+            pair[2], pair[3], pair[4], pair[0], pair[1], 
+            num_iteration, diff=True
         )
     )
 
-with open("foo.txt", "a") as myfile:
-    for line in result:
-        myfile.write(line)
-myfile.close()
-"""
-with open("output.txt", "a") as myfile:
-    for line in result:
-        myfile.write(line)
-myfile.close()
-"""
+hp.write_into_file("foo.txt", result)
